63ecb047f6fb8bf2776884e561f281409e2852df
Rutunj <rutunjp206@gmail.com>
2022-07-03 20:38:54 +0530

    fix: set serializer fields to __all__

--- tapBhomiyoAPI/serializers.py
+++ tapBhomiyoAPI/serializers.py
@@ -5,16 +5,4 @@ from .models import order_details
 class order_detailsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = order_details
-        fields = ('fullName', 'email','phone',
-'aadhar',
-'panNum',
-'postalAddress',
-'upiID',
-'whatsAppNumber',
-'instagramHandle', 
-'twitterHandle',
-'facebookHandle',
-'youtube',
-'linkedIn',
-'referenceCode',
-'birthDate')
+        fields = '__all__'
